d7806ed90f02599c5fa316ca549872ec3672696e
lihaibo <lihaibochina@gmail.com>
2018-10-25 20:10:53 +0800

    更新命令

--- index.js
+++ index.js
@@ -3,22 +3,20 @@ let args = process.argv
 let cmd = require('commander'),
   chalk = require('chalk'),
   shell = require('shelljs');
-
 /**
  * git 批量删除
  */
 cmd
   .version('0.1.0')
-  .command('branch')
   .option('-a, --all', 'delete all branch exclude master')
-  .option('-e, --exclude', 'delete exclude select branch')
-  .option('-i, --include', 'delete select branch')
+  .option('-e, --exclude', 'delete exclude branch')
+  .option('-i, --include', 'delete include branch')
   .description('批量操作git')
   .parse(args)
 
 if(cmd.all) {
   console.log(chalk.red('开始删除除master以外所有分支'))
-  shell.exec(`git branch -a | grep -vE 'master'} | xArgs git branch -D`)
+  shell.exec(`git branch -a | grep -vE master} | xArgs git branch -D`)
 }
 if(cmd.include) {
   console.log(chalk.blue('开始批量删除分支'))
