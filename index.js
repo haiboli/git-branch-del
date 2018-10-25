00ee45c77798a341616b4f28f422d8c9aadd1239
lihaibo <lihaibochina@gmail.com>
2018-10-25 19:40:01 +0800

    批量删除

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+let args = process.argv
+let cmd = require('commander'),
+  chalk = require('chalk'),
+  shell = require('shelljs');
+
+/**
+ * git 批量删除
+ */
+cmd
+  .version('0.1.0')
+  .command('branch')
+  .option('-a, --all', 'delete all branch exclude master')
+  .option('-e, --exclude', 'delete exclude select branch')
+  .option('-i, --include', 'delete select branch')
+  .description('批量操作git')
+  .parse(args)
+
+if(cmd.all) {
+  console.log(chalk.red('开始删除除master以外所有分支'))
+  shell.exec(`git branch -a | grep -vE 'master'} | xArgs git branch -D`)
+}
+if(cmd.include) {
+  console.log(chalk.blue('开始批量删除分支'))
+  shell.exec(`git branch -a | grep -E ${args[2]} | xArgs git branch -D`)
+}
+if(cmd.exclude) {
+  console.log(chalk.blue('开始批量删除其他分支'))
+  shell.exec(`git branch -a | grep -vE ${args[2]} | xArgs git branch -D`)
+}
